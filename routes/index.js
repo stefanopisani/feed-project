1c9a5899318e5f434242c267b768b4422304b2d0
Stefano Pisani <stefano.pisani92@gmail.com>
2021-04-23 14:51:08 +0100

    :)

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Recipe = require('../models/Recipe.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
