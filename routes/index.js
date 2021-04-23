dd60aa3ccc1108e17dbad0831964031b9c1df852
shinjein <shinjein@hu-berlin.de>
2021-04-23 17:47:05 +0100

    some more views

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,4 @@ router.get('/', async (req, res) => {
   });
 });
 
-
-
 module.exports = router;
--- routes/recipe.js
+++ routes/recipe.js
@@ -41,5 +41,42 @@ router.post('/add-recipe', fileUpload.single('image'), async (req, res) => {
   }
 });
 
+//edit recipe
+
+router.get('/recipe-details/:recipeId', async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.recipeId);
+    res.render('recipe-details', {
+      recipe
+    });
+  } catch (e) {
+    res.render('error');
+    console.log(`An error occurred ${e}`);
+  }
+});
+
+router.get('/edit-recipe', async (req, res) => {
+  res.render('edit-recipe');
+});
+
+router.post('/edit-recipe', fileUpload.single('image'), async (req, res) => {
+  try {
+    const fileOnCloudinary = req.file.path; // file path (url) on cloudinary
+
+    const {
+      title,
+      description
+    } = req.body;
+    await Recipe.findByIdAndUpdate({
+      title,
+      description,
+      imageUrl: fileOnCloudinary
+    });
+    res.redirect('/');
+  } catch (e) {
+    res.render('error');
+    console.log(`An error occurred ${e}`);
+  }
+});
 
 module.exports = router;
